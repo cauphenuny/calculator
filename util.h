76c817debeb219f05891d424a643247e8b81e581
hydropek <starseekist@gmail.com>
2023-11-28 22:27:51 +0800

    fixed dynamic array

--- util.h
+++ util.h
@@ -1,16 +1,20 @@
 #pragma once
 
+#include "debug.h"
+
 #define _min 1.3
 #define _max 3
 template<typename T>
-class MemoryPool {
-    T* _data;
-    uint32_t _siz, _cap;
+class Dynarray {
+    T* _data{nullptr};
+    uint32_t _siz{0}, _cap{0};
+public:
     size_t size() const {
         return _siz;
     }
     size_t reset(size_t new_n = 1) {
         delete[] _data;
+        _siz = new_n;
         _cap = new_n * _min;
         _data = new T[_cap];
         return _cap;
@@ -18,11 +22,12 @@ class MemoryPool {
     size_t resize(size_t new_n) {
         if ((size_t)(new_n * _min) > _cap || 
             (size_t)(new_n * _max) < _cap) {
+            delete[] _data;
             T* new_data = new T[(size_t)(new_n * _min)];
-            memcpy(new_data, _data, sizeof(T) * _cap);
-            _cap = (size_t)new_n * _min;
+            memcpy(new_data, _data, sizeof(T) * _siz);
+            _cap = new_n * _min;
+            _siz = new_n;
             _data = new_data;
-            delete[] new_data;
         }
         return _cap;
     }
@@ -32,22 +37,41 @@ class MemoryPool {
     const T& operator[](size_t index) const {
         return _data[index];
     }
-    MemoryPool(size_t siz = 1) {
+    Dynarray& operator= (const Dynarray& d) {
+        //debugi << "#1\n";
+        delete[] _data;
+        _siz = d._siz;
+        _cap = d._cap;
+        //debugi << "capacity: " << _cap << "\n";
+        _data = new T[_cap];
+        //debugi << "copy " << sizeof(T) * _siz << "\n";
+        memcpy(_data, d._data, sizeof(T) * _siz);
+        //debugi << "done\n";
+        return *this;
+    }
+    Dynarray& operator= (Dynarray&& d) {
+        //debugi << "#2\n";
+        delete[] _data;
+        _siz = d._siz;
+        _cap = d._cap;
+        _data = d._data;
+        d._data = nullptr;
+        //debugi << "done\n";
+        return *this;
+    }
+    Dynarray(size_t siz = 0) {
         _siz = siz;
         _cap = siz * _min;
         _data = new T[_cap];
     }
-    MemoryPool(const MemoryPool& p) {
-        _siz = p._siz;
-        _cap = p._cap;
-        _data = new T[_cap];
-        memcpy(_data, p._data, sizeof(T) * _cap);
+    Dynarray(const Dynarray& p) {
+        (*this) = p;
     }
-    MemoryPool(MemoryPool&& p) {
-        _siz = p._siz;
-        _cap = p._cap;
-        _data = p._data;
-        p._data = nullptr;
+    Dynarray(Dynarray&& p) {
+        (*this) = p;
+    }
+    ~Dynarray() {
+        delete[] _data;
     }
 };
 #undef _min
